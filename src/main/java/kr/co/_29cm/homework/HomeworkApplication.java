b5daeb236082a22654bebb07825bfff59507cd1d
고광혁 <rhkdgur195@gmail.com>
2024-06-06 08:53:25 +0900

    무신사 패키지 재 정리

--- src/main/java/kr/co/_29cm/homework/HomeworkApplication.java
+++ src/main/java/kr/co/_29cm/homework/HomeworkApplication.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -24,66 +25,65 @@ import kr.co._29cm.homework.modules.util.PayAppDisplayUtil;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@RequiredArgsConstructor
 public class HomeworkApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeworkApplication.class, args);
 	}
-	
-	@Autowired
-	private ProductService productService;
-	
-	@Autowired
-	private OrderAppService orderAppService;
+
+	private final ProductService productService;
+
+	private final OrderAppService orderAppService;
 
 	@Override
 	public void run(String... args) {
 		
 		BufferedReader 	bf = new BufferedReader(new InputStreamReader(System.in));
-		try {			
+		try {
 			while(true) {
 				String type = "";
 				System.out.print("입력(o[order]: 주문, q[quit]: 종료) : ");
 				type = bf.readLine();
-				
+
 				//주문, 종료 입력 예외처리
 				if(!"o".equals(type) && !"order".equals(type) && !"q".equals(type) && !"quit".equals(type)) {
 					System.out.println("o[order] 또는 q[quit]만 입력해주시기 바랍니다.");
 					continue;
 				}
-				
+
 				if("o".equals(type) || "order".equals(type)) {
-					
+
 					//주문 DTO
 					OrderAppDTO appDTO = new OrderAppDTO();
-					
+
 					//주문 상품 list
 					List<OrderAppItemDTO> itemList = new ArrayList<>();
 					//주문 상품번호 list
 					List<String> productNumList = new ArrayList<String>();
-					
+
 					//상품목록 조회 목록 개수 20개
 					ProductDefaultDTO searchDTO = new ProductDefaultDTO();
 					searchDTO.setSize(20);
 					Page<ProductDTO> list = productService.selectProductList(searchDTO);
 					PayAppDisplayUtil.productListDisplay(list.toList());
-					
+
 					while(true) {
 						String productNum = "";
 						String cnt = "";
-						
+
 						System.out.print("상품번호 : ");
 						productNum = bf.readLine();
-						
+
 						//상품번호가 공백이고 주문 상품이 있을 경우
-						if(productNum.isBlank() && itemList.size() > 0) {							
+						if(productNum.isBlank() && itemList.size() > 0) {
 							try {
 								//주문 정보에 주문상품 list setter
 								appDTO.setItemList(itemList);
-								orderAppService.insertOrderApp(appDTO,productNumList);	
+								orderAppService.insertOrderApp(appDTO,productNumList);
 							}catch (Exception e) {
 								System.err.println(e.getMessage());
-							}		
+							}
 							break;
 						//상품번호가 공백일 경우
 						}else if(productNum.isBlank()) {
@@ -91,7 +91,7 @@ public class HomeworkApplication implements CommandLineRunner{
 							break;
 						//주문 상품가 있는 경우
 						}else {
-							
+
 							String temp = productNum;
 							ProductDTO productDTO = list.stream().filter(x->x.getProductNum().equals(temp)).findFirst().orElse(null);
 							//상품 존재 여부
@@ -99,7 +99,7 @@ public class HomeworkApplication implements CommandLineRunner{
 								System.out.println("입력하신 상품번호는 존재하지 않습니다.");
 								continue;
 							}
-							
+
 							//중복상품 체크
 							if(itemList.size() > 0) {
 								int len = itemList.stream().filter(x->x.getProductNum().equals(temp)).collect(Collectors.toList()).size();
@@ -108,7 +108,7 @@ public class HomeworkApplication implements CommandLineRunner{
 									continue;
 								}
 							}
-							
+
 							//상품 수량 입력
 							System.out.print("수량 : ");
 							cnt = bf.readLine();
@@ -116,7 +116,7 @@ public class HomeworkApplication implements CommandLineRunner{
 								System.out.println("숫자만 입력해주시기 바랍니다. 다시 상품번호를 입력해주시기 바랍니다.");
 								continue;
 							}
-							
+
 							//주문 상품 담기
 							OrderAppItemDTO itemDTO = new OrderAppItemDTO();
 							itemDTO.setCnt(Integer.parseInt(cnt));
@@ -155,7 +155,7 @@ public class HomeworkApplication implements CommandLineRunner{
 		}catch (NumberFormatException e) {
 			return false;
 		}
-		
+
 		return true;
 	}
 }
